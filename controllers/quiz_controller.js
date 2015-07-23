5101982a9045b28c7656a9ae5158d4259a626b9c
sevillacode <sevillacode@openmailbox.org>
2015-07-23 18:43:49 +0200

    cr

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,8 @@ exports.answer = function(req, res){
 		res.render('quizes/answer', {respuesta: 'Incorrecta'});
 	}
 };
+
+// GET author
+exports.question = function(req, res){
+	res.render('author');
+};
