655e279a705bc15a1504f91b64741d40382393b5
sevillacode <sevillacode@openmailbox.org>
2015-07-27 22:29:58 +0200

    modificaciones foro 1

--- models/models.js
+++ models/models.js
@@ -1,11 +1,24 @@
 
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://usuario:password@host:puerto/database
+// Sqlite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+
 // carga modelo ORM
 var Sequelize = require('sequelize');
 
 // usa BD sqlite
-var sequelize = new Sequelize(null, null, null, {dialect: 'sqlite', storage: 'quiz.sqlite'});
+var sequelize = new Sequelize(DB_name, user, pwd, { dialect: dialect,  protocol: protocol, host: host, port: port, storage: storage, omitNull: true });
 
 // importa la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
@@ -14,12 +27,12 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz;
 
 // sequelize.sync() crea e inicializa la tabla en la BD
-sequelize.sync().success(function(){
-	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count.success(function(count){
+sequelize.sync().then(function(){
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count.then(function(count){
 		// la tabla se inicia solo si esta vacia
 		if(count === 0){
-			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).success(function(){ console.log('Base de datos actualizada') });
+			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function(){ console.log('Base de datos actualizada') });
 		}
 	});
 });
--- models/quiz.js
+++ models/quiz.js
@@ -5,6 +5,6 @@
 module.exports = function(sequelize, DataTypes){
 	return sequelize.define('Quiz', {
 								pregunta: DataTypes.STRING,
-								respuesta: DataTypes.STRING,
+								respuesta: DataTypes.STRING
 							});
 }
