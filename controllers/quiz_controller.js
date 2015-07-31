40eb1670d1b4efc82428583d684ca38ceb3d773d
sevillacode <sevillacode@openmailbox.org>
2015-07-31 23:03:52 +0200

    debug heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,6 +2,7 @@ var models = require('../models/models.js');
 
 // GET quizes/question
 exports.question = function(req, res){
+	
 	models.Quiz.findAll().then(function(quiz){
 		//console.log(quiz);
 		//process.stdout.write('quiz es: '+typeof(quiz));
--- models/models.js
+++ models/models.js
@@ -15,6 +15,9 @@ var host = (url[4] || null);
 var storage = process.env.DATABASE_STORAGE;
 
 
+process.stdout.write("DEBUG: URL DATABASE: "+DATABASE_URL+"\n");
+process.stdout.write("DEBUG: STORAGE: "+DATABASE_STORAGE+"\n");
+
 // carga modelo ORM
 var Sequelize = require('sequelize');
 
@@ -28,20 +31,29 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
 													omitNull: true 
 													});
 
+
+process.stdout.write("DEBUG: sequelize ahora está conectado\n");
+
 // importa la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 // exporta la definicion de la tabla quiz
 exports.Quiz = Quiz;
 
+
+process.stdout.write("DEBUG: Quiz ahora está conectado y exportado\n");
+
+process.stdout.write("DEBUG: Inicia la sincronizacion\n");
 // sequelize.sync() crea e inicializa la tabla en la BD
 sequelize.sync().then(function(){
+	process.stdout.write("DEBUG: sequelize ahora está sincronizado, supuestamente la tabla está creada\n");
 	// then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count.then(function(count){
-		process.stdout.write("Count: "+count+"\n");
+		process.stdout.write("DEBUG: Count: "+count+"\n");
 		// la tabla se inicia solo si esta vacia
 		if(count === 0){
-			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function(){ process.stdout.write("Base de datos actualizada\n") });
+			process.stdout.write("DEBUG: Insertará los registros dado que está vacía\n");
+			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function(){ process.stdout.write("DEBUG: Base de datos actualizada\n") });
 		}
 	});
 });
