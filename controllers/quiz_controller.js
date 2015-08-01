97c76450e155b16e2bd454cb9d2e1f86ae10bc8a
sevillacode <sevillacode@openmailbox.org>
2015-08-01 21:07:27 +0200

    v12

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,22 +16,18 @@ exports.load = function(req, res, next, quizId){
 exports.index = function(req, res, next){
 	process.stdout.write("DEBUG: entra en index para ver si muestra todos o filtra\n");
 	process.stdout.write("DEBUG: search es: "+req.query.search+"\n");
-		
-	if(req.query.search){
-		var listado = function(models,req){
-			return models.Quiz.findAll({ where: ["pregunta like %", req.query.search.replace(" ","%")]});
+	process.stdout.write("DEBUG: su tipo es: "+typeof(req.query.search)+"\n");
+	process.stdout.write("DEBUG: su longitud es: "+req.query.search.length+"\n");
+	
+	var filtro = '';	
+	if(typeof(req.query.search) != "undefined" && req.query.search.length > 0){
+			filtro = { where: ["pregunta like %", req.query.search.replace(" ","%")]};
 			process.stdout.write("DEBUG: Filtra!\n");
-		}
-	}else{
-		var listado = function(models){
-			return models.Quiz.findAll();
-			process.stdout.write("DEBUG: No filtra\n");
-		}
 	}
 	
-	listado().then(function(quizes){
+	models.Quiz.findAll(filtro).then(function(quizes){
 		
-	process.stdout.write("DEBUG: quizes: "+quizes+"\n");
+		process.stdout.write("DEBUG: quizes: "+quizes+"\n");
 		res.render('quizes/index.ejs', {quizes: quizes});
 	}).catch(function(error){ next(error); });
 }
