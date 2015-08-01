d86053bed569bf6a718b64263f1d5bdc77181ca7
sevillacode <sevillacode@openmailbox.org>
2015-08-01 15:09:03 +0200

    autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,34 +1,36 @@
 var models = require('../models/models.js');
 
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{
+				next(new Error('No existe el id: '+quizId));
+			}
+		}
+	).catch( function(error){ next(error); } );
+}
+
 exports.index = function(req, res){
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index.ejs', {quizes: quizes});
-	});
+	}).catch(function(error){ next(error); });
 }
 
 // GET quizes/:id
 exports.show = function(req, res){
-	/*
-	process.stdout.write("=============INI===============\n");
-	for (var propiedad in req.params){
-		process.stdout.write("DEBUG: "+propiedad+" : "+req.params[propiedad]+"\n");
-	}
-	process.stdout.write("=============FIN===============\n");
-	*/	
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	});
+	res.render('quizes/show', {quiz: req.quiz});
 };
 
 // GET quizes/:id/answer
 exports.answer = function(req, res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcta'});
-		}else{
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecta'});
-		}
-	});
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
 // GET author
