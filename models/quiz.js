52211a88a113e5f71713b09429039a15a2ffca25
sevillacode <sevillacode@openmailbox.org>
2015-08-02 14:32:34 +0200

    gaine

--- models/quiz.js
+++ models/quiz.js
@@ -4,7 +4,7 @@
 
 module.exports = function(sequelize, DataTypes){
 	return sequelize.define('Quiz', {
-								pregunta: {type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta pregunta"}}}
+								pregunta: {type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta pregunta"}}}, 
 								respuesta: {type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta respuesta"}}}
 							});
 }
