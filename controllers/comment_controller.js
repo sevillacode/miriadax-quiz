92ef53850b1b4b738318fb0335ae0a89d92283f0
sevillacode <sevillacode@openmailbox.org>
2015-08-15 14:21:42 +0200

    comentarios añadidos

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -2,12 +2,12 @@ var models = require('../models/models.js');
 
 // GET quizes/:quizId/comments/new
 exports.new = function(req, res){
-	res.render('comments/new.ejs', {quiz.id: req.params.quizId, errors: []});
+	res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
 }
 
 // POST quizes/:quizId/comments
 exports.create = function(req, res){
-	var comment = models.Comment.build({ texto: req.body.comment.texto, quiz.id: req.params.quizId });
+	var comment = models.Comment.build({ texto: req.body.comment.texto, QuizId: req.params.quizId });
 	
 	comment.validate().then(function(err){
 		if(err){
