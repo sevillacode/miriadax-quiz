16a1f22996e328bfc66b8d1e32bb78c922843811
sevillacode <sevillacode@openmailbox.org>
2015-08-16 14:20:19 +0200

    sesiones

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,5 @@
 var users = {
-	admin: { id: 1, username: 'admin', password: '1234' }
+	admin: { id: 1, username: 'admin', password: '1234' },
 	pepe:  { id: 2, username: 'pepe', password: '5678' }
 };
 
