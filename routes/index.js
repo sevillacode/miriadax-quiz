818ed81cb0544032478d8bf9c3f1a8883cebd1ad
sevillacode <sevillacode@openmailbox.org>
2015-08-16 15:32:58 +0200

    moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 
 // autoload de comandos con quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 router.get('/login', sessionController.new); // formulario login
 router.post('/login', sessionController.create); // crear
@@ -30,6 +31,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', quizController.author);
 
